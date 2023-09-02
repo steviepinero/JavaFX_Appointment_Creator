39e229a4029d1e69ac8e2023d8c514fb19eb2376
stevie <steviepinero@gmail.com>
2023-09-02 18:11:17 -0400

    reports will only load when the tab is selected. Need to fix nullpointerexception for some of the cell values

--- src/main/java/com/javafx_project/controllers/ReportController.java
+++ src/main/java/com/javafx_project/controllers/ReportController.java
@@ -3,6 +3,7 @@ package com.javafx_project.controllers;
 import com.javafx_project.dao.DatabaseConnection;
 import com.javafx_project.models.Appointment;
 import com.javafx_project.models.Contact;
+import com.javafx_project.models.User;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
@@ -10,24 +11,21 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-public class ReportController {
+public class ReportController implements Initializable {
 
     /**  the total number of customer appointments by type and month
 
@@ -37,26 +35,48 @@ public class ReportController {
      */
 
     @FXML
-    private TableView<Map.Entry<String, Map<String, Integer>>> reportTable1;
+    private TableView<Map.Entry<String, Map<String, Integer>>> customerReportTable;
     @FXML
-    private TableColumn<Map.Entry<String, Map<String, Integer>>, String> monthColumn;
+    private TableColumn<Map.Entry<String, Map<String, Integer>>, String> appointmentMonthColumn;
     @FXML
     private TableColumn<Map.Entry<String, Map<String, Integer>>, String> typeColumn;
     @FXML
     private TableColumn<Map.Entry<String, Map<String, Integer>>, Integer> appointmentCountColumn;
 
     @FXML
-    private TableView<Appointment> reportTable2;
+    private TableView<Appointment> contactReportTable;
     @FXML
     private TableColumn<Appointment, Integer> appointmentIdColumn;
     @FXML
     private TableColumn<Appointment, String> titleColumn;
 
     @FXML
+    private TableView<User> userReportTable;
+    @FXML
+    private TableColumn<User, String> usernameColumn;
+    @FXML
+    private TableColumn<User, Integer> updateCountColumn;
+
+    @FXML
     private Button backButton;
+
+    @FXML
+    private TabPane reportTabPane;
+
+    @FXML
+    private Tab customerReportTab;
+
+    @FXML
+    private Tab contactReportTab;
+
+    @FXML
+    private Tab userReportTab;
+    private TableColumn<Object, Object> customerNameColumn;
 
     //report that tracks the total number of customer appointments by type and month
     public Map<String, Map<String, Integer>> getAppointmentsByTypeAndMonth() {
+        DatabaseConnection.establishConnection();
+
         String query = "SELECT MONTHNAME(Start) as Month, Type, COUNT(*) as Count FROM appointments GROUP BY MONTH(Start), Type";
         Map<String, Map<String, Integer>> resultMap = new HashMap<>();
 
@@ -78,6 +98,8 @@ public class ReportController {
 
     //report/schedule that tracks the total number of appointments by contact
     public Map<Contact, List<Appointment>> getScheduleForContacts() {
+        DatabaseConnection.establishConnection();
+
         String query = "SELECT * FROM appointments ORDER BY Contact_ID";
         Map<Contact, List<Appointment>> resultMap = new HashMap<>();
 
@@ -107,8 +129,12 @@ public class ReportController {
 
     // report that tracks the total number of updates made by user
     public void userUpdateReport() {
+        DatabaseConnection.establishConnection();
+
         // report that tracks the number of updates made by each user
 
+        usernameColumn.setCellValueFactory(new PropertyValueFactory<>("User_Name"));
+        updateCountColumn.setCellValueFactory(new PropertyValueFactory<>("Update_Count"));
 
 
 
@@ -117,25 +143,31 @@ public class ReportController {
 
     //displays the report that tracks the total number of customer appointments by type and month
     public void customerAppointmentReport() {
+        DatabaseConnection.establishConnection();
+
         Map<String, Map<String, Integer>> data = getAppointmentsByTypeAndMonth();
 
-        monthColumn.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().getKey()));
+        customerNameColumn.setCellValueFactory(new PropertyValueFactory<>("Customer_Name"));
+        appointmentMonthColumn.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().getKey()));
         typeColumn.setCellValueFactory(p -> new SimpleStringProperty(p.getValue().getValue().keySet().toString()));
         appointmentCountColumn.setCellValueFactory(p -> new SimpleIntegerProperty(p.getValue().getValue().values().stream().mapToInt(Integer::intValue).sum()).asObject());
 
         ObservableList<Map.Entry<String, Map<String, Integer>>> items = FXCollections.observableArrayList(data.entrySet());
-        reportTable1.setItems(items);
+        customerReportTable.setItems(items);
     }
 
     //displays the report that tracks the total number of appointments by contact
-    public void contactAppointmentReport(Contact contact) {
+    public void contactAppointmentReport() {
+        DatabaseConnection.establishConnection();
+
         Map<Contact, List<Appointment>> data = getScheduleForContacts();
 
         appointmentIdColumn.setCellValueFactory(new PropertyValueFactory<>("appointment_ID"));
         titleColumn.setCellValueFactory(new PropertyValueFactory<>("title"));
 
+        Contact contact = new Contact();
         ObservableList<Appointment> items = FXCollections.observableArrayList(data.get(contact));
-        reportTable2.setItems(items);
+        contactReportTable.setItems(items);
     }
 
     public void backButtonAction(ActionEvent actionEvent) throws IOException {
@@ -150,5 +182,29 @@ public class ReportController {
         // Create new scene and set it on the stage
         Scene scene = new Scene(root);
         stage.setScene(scene);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        DatabaseConnection.getConnection();
+
+        // Set up tab selection listeners
+        customerReportTab.setOnSelectionChanged(event -> {
+            if (customerReportTab.isSelected()) {
+                customerAppointmentReport();
+            }
+        });
+
+        contactReportTab.setOnSelectionChanged(event -> {
+            if (contactReportTab.isSelected()) {
+                contactAppointmentReport();
+            }
+        });
+
+        userReportTab.setOnSelectionChanged(event -> {
+            if (userReportTab.isSelected()) {
+                userUpdateReport();
+            }
+        });
     }
 }
--- src/main/java/com/javafx_project/models/Report.java
+++ src/main/java/com/javafx_project/models/Report.java
@@ -1,4 +1,41 @@
 package com.javafx_project.models;
 
 public class Report {
+    private String type;
+    private String month;
+    private int count;
+
+    public Report(String type, String month, int count) {
+        this.type = type;
+        this.month = month;
+        this.count = count;
+    }
+
+    public Report() {
+        // No-argument constructor
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
 }
